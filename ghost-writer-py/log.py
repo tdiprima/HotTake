34cc84eac8c0471ac6ac03c4ab881d7f4041ee19
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-10-28 08:19:28 -0400

    Update log.py

--- ghost-writer-py/log.py
+++ ghost-writer-py/log.py
@@ -10,7 +10,7 @@ def log_article(title, keywords):
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive",
     ]
-    creds = Credentials.from_service_account_file("creds.json", scopes=scope)
+    creds = Credentials.from_service_account_file("../creds.json", scopes=scope)
     client = gspread.authorize(creds)
     sheet = client.open("Blog Stats").sheet1
     sheet.append_row([title, ", ".join(keywords)])
